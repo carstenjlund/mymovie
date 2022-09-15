4da64871847f12631536ef12accdc498310d85c0
Carsten Lund <clu@rts.dk>
2022-09-15 15:43:44 +0200

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+const Header = () => {
+	return (
+		<header className="header">
+			<h1>MyMovie</h1>
+			<button>switch</button>
+		</header>
+	);
+};
+
+export default Header;
