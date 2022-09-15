9736c1a528a2c3132bf7a73d2ca0a94164407d5e
Carsten Lund <clu@rts.dk>
2022-09-15 15:44:04 +0200

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import Navigation from "./Navigation";
+import "./footer.css";
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<Navigation />
+		</footer>
+	);
+};
+
+export default Footer;
