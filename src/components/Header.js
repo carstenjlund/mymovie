fcca2c06f3d5bec3267ea60c389d4120decc7d48
Carsten Lund <clu@rts.dk>
2022-09-23 15:07:02 +0200

    updated header to handle subpage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import "./header.css";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 const Header = () => {
+	const location = useLocation();
 	return (
-		<header className="header">
+		<header
+			className={`header ${
+				location.pathname.startsWith("/detail") && "subpage"
+			}`}
+		>
+			{location.pathname.startsWith("/detail") && (
+				<Link to="/">&larr;</Link>
+			)}
 			<h1>MyMovie</h1>
 			<button>switch</button>
 		</header>
