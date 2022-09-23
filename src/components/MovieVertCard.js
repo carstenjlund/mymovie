04797b8110ca7b89485f50ceb5881b412995ac10
Carsten Lund <clu@rts.dk>
2022-09-23 15:07:59 +0200

    updated vertical card to link to detailspage

--- src/components/MovieVertCard.js
+++ src/components/MovieVertCard.js
@@ -1,15 +1,18 @@
 import "./movievertcard.css";
-const MovieVertCard = ({ image, title, rating }) => {
+import { Link } from "react-router-dom";
+const MovieVertCard = ({ image, title, rating, id }) => {
 	return (
-		<article className="movieVertCard">
+		<Link to={`/detail/${id}`} className="movieVertCard">
 			<img
 				src={`https://image.tmdb.org/t/p/w500${image}`}
 				alt={`${title} poster`}
 				loading="lazy"
 			/>
 			<h3>{title}</h3>
-			<p className="movieVertCard__rating"><i className="fa-solid fa-star"></i> {rating}/10 IMDb</p>
-		</article>
+			<p className="movieVertCard__rating">
+				<i className="fa-solid fa-star"></i> {rating}/10 IMDb
+			</p>
+		</Link>
 	);
 };
 
