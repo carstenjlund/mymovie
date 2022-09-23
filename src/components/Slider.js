60cc2af061f8572997b6fe7ad9889ee47484da90
Carsten Lund <clu@rts.dk>
2022-09-23 15:26:24 +0200

    extracted css and removed scrollbar

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,26 +1,21 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import MovieVertCard from "./MovieVertCard";
+import "./slider.css";
+
 const Slider = () => {
 	const [movies, setmovies] = useState();
 
 	useEffect(() => {
 		axios(
-			"https://api.themoviedb.org/3/movie/now_playing?language=da-DK&api_key=c28b09251184479f999a2baafd615444"
+			"https://api.themoviedb.org/3/movie/now_playing?language=en-US&api_key=c28b09251184479f999a2baafd615444"
 		).then((response) => setmovies(response.data.results));
 	}, []);
 
 	return !movies ? (
 		<h3>Loading...</h3>
 	) : (
-		<section
-			style={{
-				display: "flex",
-				overflowX: "scroll",
-				gap: "1.25rem",
-				margin: "1rem 0 2.5rem",
-			}}
-		>
+		<section className="slider">
 			{movies.map((movie) => (
 				<MovieVertCard
 					key={movie.id}
